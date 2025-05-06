94b6dcaac065313ef299cdd5c935e5d51b544880
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 00:56:46 +0200

    added LoginPage selectors

--- src/main/java/com/take_home/pages/LoginPage.java
+++ src/main/java/com/take_home/pages/LoginPage.java
@@ -1,5 +1,24 @@
 package com.take_home.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
-    
+    @FindBy(id = "user-name")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(css = "h3[data-test='error']")
+    private WebElement errorMessage;
+
+    public void login(String username, String password) {
+        sendKeys(usernameField, username);
+        sendKeys(passwordField, password);
+        click(loginButton);
+    }
 }
