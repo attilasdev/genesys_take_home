85aedd8ea9d98da49ee07eb8469eca77f42bc288
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 00:41:00 +0200

    added @BeforeEach and @AfterEach

--- src/test/java/com/sdet/tests/BaseTest.java
+++ src/test/java/com/sdet/tests/BaseTest.java
@@ -1,5 +1,21 @@
 package com.sdet.tests;
 
+import com.take_home.config.DriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BaseTest {
-    
+    protected static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
+
+    @BeforeEach
+    public void setUp() {
+        DriverManager.getDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        DriverManager.quitDriver();
+    }
 }
