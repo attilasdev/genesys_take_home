d8df991c3168971b9184253d3203636d2af81039
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 00:45:40 +0200

    added credentials

--- src/main/java/com/take_home/models/Credentials.java
+++ src/main/java/com/take_home/models/Credentials.java
@@ -1,5 +1,11 @@
 package com.take_home.models;
 
 public class Credentials {
-    
+    private String username;
+    private String password;
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
