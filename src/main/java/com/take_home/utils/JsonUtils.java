6d67daef0b38f94c64f5bc77b6a5b2ff001ad0b4
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 00:27:38 +0200

    added readJsonFile, parseJson

--- src/main/java/com/take_home/utils/JsonUtils.java
+++ src/main/java/com/take_home/utils/JsonUtils.java
@@ -1,5 +1,17 @@
 package com.take_home.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+
 public class JsonUtils {
-    
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static <T> T readJsonFile(String filePath, Class<T> valueType) throws IOException {
+        return objectMapper.readValue(new File(filePath), valueType);
+    }
+
+    public static <T> T parseJson(String json, Class<T> valueType) throws IOException {
+        return objectMapper.readValue(json, valueType);
+    }
 }
