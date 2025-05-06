b25a1f77be3e819454718c79aba0e367c2ff5473
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 00:32:32 +0200

    waitForElementVisible

--- src/main/java/com/take_home/pages/BasePage.java
+++ src/main/java/com/take_home/pages/BasePage.java
@@ -1,5 +1,25 @@
 package com.take_home.pages;
 
+import com.take_home.config.DriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
 public class BasePage {
-    
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage() {
+        this.driver = DriverManager.getDriver();
+        this.wait = new WebDriverWait(driver, 15);
+        PageFactory.initElements(driver, this);
+    }
+
+    protected void waitForElementVisible(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
 }
