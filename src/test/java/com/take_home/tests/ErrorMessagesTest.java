2b4a4f56100ef6f1d9609789079c774034e574ba
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 11:30:44 +0200

    added TestCase1(PurchaseProcessTest) and TestCase2(ErrorMessageTest)

--- src/test/java/com/take_home/tests/ErrorMessagesTest.java
+++ src/test/java/com/take_home/tests/ErrorMessagesTest.java
@@ -1,12 +1,32 @@
 package com.take_home.tests;
 
 import com.take_home.pages.InventoryPage;
-import com.take_home..pages.LoginPage;
+import com.take_home.pages.LoginPage;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junist.jupter.apit.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ErrorMessagesTest {
+public class ErrorMessagesTest extends BaseTest{
     
+    @Test
+    public void testErrorMessagesAndFooter() {
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.open();
+
+        loginPage.clickLoginButton();
+
+        String errorMsg = loginPage.getErrorMessage();
+        assertEquals("Epic sadface: Username is required", errorMsg, "Error Message should match.");
+        
+        loginPage.login("standard_user", "secret_sauce");
+
+        InventoryPage inventoryPage = new InventoryPage();
+        String footerText =  inventoryPage.getFooterText();
+
+        assertTrue(footerText.contains("2025"), "Footer should contain the years 2025.");
+        assertTrue(footerText.contains("Terms of Service"), "Footer should contain Terms of Service.");
+    }
 }
