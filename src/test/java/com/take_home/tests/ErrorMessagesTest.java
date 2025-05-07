69303f9b3683080acf828df3eab9d81f8d56301e
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 11:26:56 +0200

    added errormessagetest login and assert

--- src/test/java/com/take_home/tests/ErrorMessagesTest.java
+++ src/test/java/com/take_home/tests/ErrorMessagesTest.java
@@ -1,5 +1,12 @@
 package com.take_home.tests;
 
+import com.take_home.pages.InventoryPage;
+import com.take_home..pages.LoginPage;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junist.jupter.apit.Assertions.assertTrue;
+
 public class ErrorMessagesTest {
     
 }
