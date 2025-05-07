a4cb48c0076cf145518d8aaa94a2232e65b9b3bd
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 10:15:07 +0200

    added login to Test

--- src/test/java/com/sdet/tests/PurchaseProcessTest.java
+++ src/test/java/com/sdet/tests/PurchaseProcessTest.java
@@ -1,5 +1,27 @@
 package com.sdet.tests;
 
-public class PurchaseProcessTest {
+import com.take_home.models.Credentials;
+import com.take_home.pages.CartPage;
+import com.take_home.pages.CheckoutPage;
+import com.take_home.pages.InventoryPage;
+import com.take_home.pages.LoginPage;
+import com.take_home.utils.JsonUtils;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PurchaseProcessTest extends BaseTest {
     
+    @Test
+    public void testPurchaseProcess() throws IOException {
+        String filePath = getClass().getClassLoader().getResource("credentials.json").getPath();
+        Credentials credentials = JsonUtils.readJsonFile(filePath, Credentials.class);
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.open();
+        loginPage.login(credentials.getUsername(), credentials.getPassword());
+
+        
+    }
 }
