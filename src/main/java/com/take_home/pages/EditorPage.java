8512bb342db9a9c32e6733b7c1f1e31680f9803e
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 12:07:23 +0200

    added EditorPage selectors

--- src/main/java/com/take_home/pages/EditorPage.java
+++ src/main/java/com/take_home/pages/EditorPage.java
@@ -1,5 +1,18 @@
 package com.take_home.pages;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
 public class EditorPage {
+    @FindBy(css = ".ql-editor")
+    private WebElement editor;
+
+    @FindBy(css = ".ql-bold")
+    private WebElement boldButton;
+
+    @FindBy(css = ".ql-underline")
+    private WebElement underlineButton;
     
 }
