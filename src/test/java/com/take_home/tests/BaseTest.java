91bc137e3de74fdb07f36bdccd1fcb42e915ccd0
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 15:53:00 +0200

    added more logging

--- src/test/java/com/take_home/tests/BaseTest.java
+++ src/test/java/com/take_home/tests/BaseTest.java
@@ -5,17 +5,23 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.openqa.selenium.WebDriver;
+import org.junit.jupiter.api.TestInfo;
+
 
 public class BaseTest {
+    protected WebDriver driver;
     protected static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
 
     @BeforeEach
-    public void setUp() {
+    public void setUp(TestInfo testInfo) {
+        logger.info("==== STARTING TEST: {} ====", testInfo.getDisplayName());
         DriverManager.getDriver();
     }
 
     @AfterEach
-    public void tearDown() {
+    public void tearDown(TestInfo testInfo) {
+        logger.info("==== COMPLETED TEST: {} ====", testInfo.getDisplayName());
         DriverManager.quitDriver();
     }
 }
