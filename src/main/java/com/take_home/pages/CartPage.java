883d34f31ad4f77c3e2ad7575a35bd1d09019507
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 09:50:13 +0200

    added CartPage

--- src/main/java/com/take_home/pages/CartPage.java
+++ src/main/java/com/take_home/pages/CartPage.java
@@ -1,5 +1,16 @@
 package com.take_home.pages;
 
-public class CartPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+    public class CartPage extends BasePage {
+        @FindBy(id = "checkout") 
+        private WebElement checkoutButton;
+
+        public void proceedToCheckout() {
+            click(checkoutButton);
+        }
     
-}
+    }
+
