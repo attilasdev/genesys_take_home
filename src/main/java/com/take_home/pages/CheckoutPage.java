e2c642911c0e55cf6d1c8a9360639dd1a3771ae9
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 09:50:37 +0200

    added CheckoutPage selectors

--- src/main/java/com/take_home/pages/CheckoutPage.java
+++ src/main/java/com/take_home/pages/CheckoutPage.java
@@ -1,5 +1,26 @@
 package com.take_home.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class CheckoutPage {
+    @FindBy(id = "first-name")
+    private WebElement firstNameField;
+
+    @FindBy(id = "last-name")
+    private WebElement lastNameField;
+
+    @FindBy(id = "postal-code")
+    private WebElement postalCodeField;
+
+    @FindBy(id = "continue")
+    private WebElement continueButton;
+
+    @FindBy(id = "finish")
+    private WebElement finishButton;
+
+    @FindBy(className = "complete-header")
+    private WebElement confirmationMessage;
+
     
 }
