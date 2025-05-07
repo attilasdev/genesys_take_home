e5ac14b7bfccd87912e2e4c2fbec64f41c6310c0
attilasdev <60004003+attilasdev@users.noreply.github.com>
2025-05-07 09:14:02 +0200

    added addItemToCart

--- src/main/java/com/take_home/pages/InventoryPage.java
+++ src/main/java/com/take_home/pages/InventoryPage.java
@@ -1,5 +1,22 @@
 package com.take_home.pages;
 
-public class InventoryPage {
-    
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class InventoryPage extends BasePage {
+    @FindBy(id = "shopping_cart_container")
+    private WebElement cartBadge;
+
+    @FindBy(className = "shopping_cart_link")
+    private WebElement cartLink;
+
+    @FindBy(css = ".footer")
+    private WebElement footer;
+
+    public void addItemToCart(String itemName) {
+        String xpath = String.format("//div[text()='%s']/ancestor::div[@class='inventory_item']//button", itemName);
+        WebElement addButton = driver.findElement(By.xpath(xpath));
+        click(addButton);
+    }
 }
